dca03b6763679fd6bf70377304b5af00def2b07a
Divya Vinay <divya.basappa@gmail.com>
2017-09-06 18:47:48 -0700

    Fixed alignment issues and changed title color

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -15,9 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         UINavigationBar.appearance().barTintColor = UIColor.hexStringToUIColor(hex: "A9CE41")
-        UINavigationBar.appearance().titleTextAttributes = [ NSFontAttributeName: UIFont(name: "Chalkduster", size: 20)!]
-        UINavigationBar.appearance().tintColor = UIColor.white
-        
+        UINavigationBar.appearance().titleTextAttributes = [ NSFontAttributeName: UIFont(name: "Chalkduster", size: 20)!, NSForegroundColorAttributeName: UIColor.white]
+       
         return true
     }
 
